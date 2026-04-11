6168cb10d8d4f960c56568c614d864e6bc80ac41
raydanielg <airezra2+1@gmail.com>
2026-04-11 22:17:05 +0300

    commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'icon',
+        'image',
+        'sort_order',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
