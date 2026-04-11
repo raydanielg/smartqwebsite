549c637bf606668cafdaecba2e0162d796d6f395
raydanielg <airezra2+1@gmail.com>
2026-04-11 23:21:45 +0300

    commit

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'description',
+        'group',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'permission_role');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function scopeByGroup($query, $group)
+    {
+        return $query->where('group', $group);
+    }
 }
