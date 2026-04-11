4b8baa0d6e838dac92100d2a3359b2e5d90bea63
raydanielg <airezra2+1@gmail.com>
2026-04-11 23:06:33 +0300

    commit

--- app/Models/TaxExemption.php
+++ app/Models/TaxExemption.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class TaxExemption extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'business_name',
+        'tax_id_number',
+        'business_type',
+        'business_address',
+        'city',
+        'country',
+        'zip_code',
+        'phone',
+        'email',
+        'tax_exemption_certificate',
+        'certificate_number',
+        'certificate_expiry',
+        'status',
+        'notes',
+        'approved_at',
+        'approved_by',
+    ];
+
+    protected $casts = [
+        'certificate_expiry' => 'date',
+        'approved_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
 }
