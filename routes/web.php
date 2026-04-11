600c19d980ea4a2054ab7e8277e42a8c15203402
raydanielg <airezra2+1@gmail.com>
2026-04-11 22:21:58 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Landing Page
+Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 Auth::routes();
 
