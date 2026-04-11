7b0e1a4d6b7b57ee33979bf8a8bc8cc42687d53e
raydanielg <airezra2+1@gmail.com>
2026-04-11 23:05:43 +0300

    commit

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Manufacturer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+        'description',
+        'country',
+        'city',
+        'business_type',
+        'main_products',
+        'year_established',
+        'total_employees',
+        'certifications',
+        'is_verified',
+        'verification_level',
+        'contact_person',
+        'phone',
+        'email',
+        'website',
+        'response_rate',
+        'transactions_count',
+        'is_active',
+        'sort_order',
+    ];
+
+    protected $casts = [
+        'is_verified' => 'boolean',
+        'is_active' => 'boolean',
+        'response_rate' => 'decimal:2',
+        'year_established' => 'integer',
+        'total_employees' => 'integer',
+        'transactions_count' => 'integer',
+    ];
 }
